7603916ceb3dc2168ac3fb0dc5825d7785dc7644
Bryan Lee <38807139+liby@users.noreply.github.com>
2025-01-19 03:08:43 +0800

    Configure development environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    hmr: true,
+    watch: {
+      usePolling: true,
+    },
+  },
 })
